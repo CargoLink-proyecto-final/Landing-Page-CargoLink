7652a3fdb803d9572cb86d34176c2538db164e1c
mfrias42 <friasmanuel42@gmail.com>
2026-04-14 17:49:25 -0300

    fix: remove GitHub link from Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ export default function Footer() {
       <div className="container footer-inner">
         <div className="footer-brand">Cargo<span>Link</span></div>
         <ul className="footer-links">
-          <li><a href="https://github.com/vicent301/CargoLink.git" target="_blank" rel="noopener">GitHub</a></li>
           <li><a href="#encuestas">Encuestas</a></li>
           <li><a href="#equipo">Equipo</a></li>
         </ul>
